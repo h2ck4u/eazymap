763b63726c624f8a3bf8a6acfb9e05d8592190e5
jeungeuny <h2ckforu@gmail.com>
2021-09-13 16:33:54 +0900

    :pencil: webpack 설정 및 d3 import

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,13 @@
-function test(str:string) {
-  console.log(`hello easymap ${str}`);
-};
+import * as d3 from 'd3';
 
-test('test');
+const width = 975;
+const height = 610;
+
+const svg = d3.create('svg')
+.attr('width', width)
+.attr('height', height);
+
+const backgrond = svg.append('path');
+const borders = svg.append('path');
+
+document.body.appendChild(svg.node() as SVGSVGElement);
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const webpack = require('webpack');
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+const config = {
+  entry: './src/index.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts(x)?$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: [
+      '.tsx',
+      '.ts',
+      '.js'
+    ]
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [{ from: 'src/index.html' }],
+    }),
+    new HtmlWebpackPlugin({
+      templateContent: ({ htmlWebpackPlugin }) => '<!DOCTYPE html><html><head><meta charset=\"utf-8\"><title>' + htmlWebpackPlugin.options.title + '</title></head><body><div id=\"app\"></div></body></html>',
+      filename: 'index.html',
+    }),
+    new CleanWebpackPlugin()
+  ]
+};
+
+module.exports = config;
