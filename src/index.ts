d2ce70c93e3224a1a4735a8a5be6258bd62ba908
jeungeuny <h2ckforu@gmail.com>
2021-09-29 14:21:52 +0900

    :green_heart: 한국지도 그리기

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,44 @@
 import * as d3 from 'd3';
+import * as topoclient from 'topojson-client';
 
-const width = 975;
-const height = 610;
+window.onload = function () {
+  drawMap();
+};
 
-const svg = d3.create('svg')
-  .attr('width', width)
-  .attr('height', height);
+function drawMap() {
+  const koreaMap = require('../public/korea.json');
+  const geojson = topoclient.feature(koreaMap, koreaMap.objects.skorea_provinces_2018_geo) as any;
+  const center = d3.geoCentroid(geojson);
 
-const backgrond = svg.append('path')
-  .attr('fill', '#ddd');
+  const width = 600;
+  const height = 1000;
+  const svg = d3
+    .select('.d3')
+    .append('svg')
+    .attr('width', width).attr('height', height);
+  const map = svg.append('g');
 
-  const borders = svg.append('path')
- .attr('fill', 'none')
- .attr('stroke', '#fff')
- .attr('stroke-linejoin', 'round')
- .attr('stroke-linecap', 'round');
+  let projection = d3.geoMercator()
+    .scale(1)
+    .translate([0, 0])
 
-document.body.appendChild(svg.node() as SVGSVGElement);
+  const path = d3.geoPath().projection(projection) as any;
+  const bounds = path.bounds(geojson);
+  const widthScale = (bounds[1][0] - bounds[0][0]) / width;
+  const heightScale = (bounds[1][1] - bounds[0][1]) / height;
+  const scale = 1 / Math.max(widthScale, heightScale);
+  const xoffset = width / 2 - scale * (bounds[1][0] + bounds[0][0]) / 2 + 10;
+  const yoffset = height / 2 - scale * (bounds[1][1] + bounds[0][1]) / 2 + 80;
+  projection.scale(scale).translate([xoffset, yoffset]);
+
+  map
+    .selectAll('path').data(geojson.features)
+    .enter().append('path')
+    .attr('d', path);
+
+  // const zoomed = () => {
+  //   map.attr('transform', d3.geoTransform)
+  // }
+  // const zoom = d3.zoom().scaleExtent([1, 8]).on('zoom', zoomed)
+  // svg.call(zoom)
+}
